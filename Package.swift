2ab81cf3bb08691d7b25964e44becd7c7c40983b
Daniel Resting <danielresting@Hurricane.local>
2019-05-20 15:16:41 -0400

    it builds

--- Package.swift
+++ Package.swift
@@ -7,20 +7,21 @@ let package = Package(
     name: "IAPServer",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
+        .executable(
             name: "IAPServer",
-            targets: ["IAPServer"]),
+            targets: ["IAPServerRun"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/vapor/vapor", from: "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "IAPServerRun", dependencies: ["IAPServer", "Vapor"]),
         .target(
             name: "IAPServer",
-            dependencies: []),
+            dependencies: ["Vapor"]),
         .testTarget(
             name: "IAPServerTests",
             dependencies: ["IAPServer"]),
---
+++ Sources/IAPServerRun/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  IAPServer
+//
+//  Created by Daniel Resting on 5/20/19.
+//
+
+import Foundation
+import Vapor
+import IAPServer
