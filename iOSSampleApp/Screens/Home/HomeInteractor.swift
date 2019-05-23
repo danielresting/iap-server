4aee51097e06cb9398f5c06228e54410c538d181
Daniel Resting <danielresting@gmail.com>
2019-05-23 17:40:48 -0400

    start interacting with swifty store kit

--- iOSSampleApp/Screens/Home/HomeInteractor.swift
+++ iOSSampleApp/Screens/Home/HomeInteractor.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftyStoreKit
 
 class HomeInteractor {
     weak var output: HomeInteractorOutput?
@@ -14,7 +15,25 @@ class HomeInteractor {
 
 extension HomeInteractor: HomeInteractorInput {
     func subscribe() {
-        
+        SwiftyStoreKit.purchaseProduct("com.musevisions.SwiftyStoreKit.Purchase1", quantity: 1, atomically: true) { result in
+            switch result {
+            case .success(let purchase):
+                print("Purchase Success: \(purchase.productId)")
+            case .error(let error):
+                switch error.code {
+                case .unknown: print("Unknown error. Please contact support")
+                case .clientInvalid: print("Not allowed to make the payment")
+                case .paymentCancelled: break
+                case .paymentInvalid: print("The purchase identifier was invalid")
+                case .paymentNotAllowed: print("The device is not allowed to make the payment")
+                case .storeProductNotAvailable: print("The product is not available in the current storefront")
+                case .cloudServicePermissionDenied: print("Access to cloud service information is not allowed")
+                case .cloudServiceNetworkConnectionFailed: print("Could not connect to the network")
+                case .cloudServiceRevoked: print("User has revoked permission to use this cloud service")
+                default: print((error as NSError).localizedDescription)
+                }
+            }
+        }
     }
     
     func fetchServerStatus() {
@@ -22,6 +41,6 @@ extension HomeInteractor: HomeInteractorInput {
     }
     
     func fetchLocalStatus() {
-        
+        output?.deliver(localStatus: .inactive)
     }
 }
